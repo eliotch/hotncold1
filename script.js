eb5335bb4b84ce0af18437e9f6ec992fcb0c4380
eliotch <eliotc@nycstudents.net>
2023-10-05 11:06:13 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+function startGame() {
+  attempts++;
+  const userGuess = prompt("Guess a number between 1 and 100:");
+  if (parseInt(userGuess) === randomNumber) {
+    alert(`Congratulations! You guessed the number ${randomNumber} in ${attempts} attempts.`);
+  } else {
+    const hint = userGuess < randomNumber ? "higher" : "lower";
+    alert(`Try again! The number is ${hint} than your guess.`);
+    startGame();
+  }
+}
+
+startGame();
